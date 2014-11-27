2153bea44fcc818abfba8edde80dfdf1b9405e2e
greatPurpose <happy.goodday@outlook.com>
2014-11-27 09:26:12 +0900

    serializers.py added

---
+++ app/product/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from core.models import Tag, Product, Coverage
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class CoverageSerializer(serializers.ModelSerializer):
+    """Serializer for coverage objects"""
+
+    class Meta:
+        model = Coverage
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    """Serializer for Product objects"""
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Product
+        fields = ('id', 'title', 'tags', 'price', 'link')
+        read_only_fields = ('id',)
+
+
+class ProductDetailSerializer(ProductSerializer):
+    """Serializer for Product details"""
+    tags = TagSerializer(many=True, read_only=True)
